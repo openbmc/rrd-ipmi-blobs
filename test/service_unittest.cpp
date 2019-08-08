007aa22ef16e82f293417ca69440626979195350
Gabriel Matute <gmatute@google.com>
2019-08-08 15:32:02 -0700

    Add tests for RrdService::handle method delegation.
    Currently, tests only check that each method (i.e. last) is capable of
    properly handling a request and that handle is able to signal an error
    if there is an unparsable buffer or a request with an unset method. This
    change uses a RrdService mock to check the interaction between the
    handle method and the service object.
    
    Signed-off-by: Gabriel Matute <gmatute@google.com>
    Google-Bug-Id: 137037681
    Change-Id: I121102cf9ff87b12b29051627de9a104ee9b4169

--- test/service_unittest.cpp
+++ test/service_unittest.cpp
@@ -1,6 +1,44 @@
 #include "service_unittest.hpp"
 
-TEST_F(RrdServiceTest, HandleInvalid)
+using ::testing::_;
+using ::testing::Return;
+
+namespace rrd
+{
+class RrdServiceHandleMock : public RrdService
+{
+  public:
+    using RrdService::RrdService;
+
+    // Mock every method but the handle overload set.
+    MOCK_METHOD2(last, bool(const LastRequest& req, LastResponse& res));
+};
+} // namespace rrd
+
+class RrdServiceHandleTest : public RrdServiceTest
+{
+  protected:
+    rrd::RrdServiceHandleMock service{&rrd};
+};
+
+// NOTE: the following test the interaction between RrdService::handle and the
+// rest of the interface provided by RrdService. The handle method should always
+// delegate a request to the appropriate method or signal an error.
+
+TEST_F(RrdServiceHandleTest, HandleLast)
+{
+    EXPECT_CALL(service, last(_, _)).Times(2).WillRepeatedly(Return(true));
+
+    req.mutable_last();
+    ASSERT_TRUE(service.handle(req, res))
+        << "Handle request with last method set";
+
+    req.SerializeToString(&reqBuffer);
+    ASSERT_TRUE(service.handle(reqBuffer, resBuffer))
+        << "Handle buffer with last method set";
+}
+
+TEST_F(RrdServiceHandleTest, HandleInvalid)
 {
     // Test overloaded handle with invalid input
 
